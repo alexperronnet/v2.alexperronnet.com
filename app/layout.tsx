e14d2faa79982350d991932c53884c352cc8f0c9
alexperronnet <github.tkqeh@apmails.com>
2025-10-28 01:06:46 +0100

    feat: implement design system

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,7 @@
 import "@/styles/globals.css";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { cn } from "@/libs/class-names";
+import { fontVariables } from "@/libs/fonts";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -19,9 +10,12 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
-    <html lang="en">
+    <html className="scroll-smooth" lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={cn(
+          "overscroll-none bg-base font-sans text-text antialiased",
+          fontVariables
+        )}
       >
         {children}
       </body>
